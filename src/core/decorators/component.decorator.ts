159f0fbf40c0a56e887056d9273099441d6dfc5f
Peter van der Wal <peterzakelijk@live.nl>
2025-01-03 15:56:11 +0100

    add some docs

--- src/core/decorators/component.decorator.ts
+++ src/core/decorators/component.decorator.ts
@@ -6,6 +6,8 @@ export function Component(config:{selector: string, templateUrl: string}){
         target.selector = config.selector;
         const originalConstructor = target;
 
+        //Attach the templatePromise to the instance instead of the shared prototype.
+        //This makes it so that each instance of the class gets its own property.
         const newConstructor: any = function(...args: any[]){
             // Call the original constructor
             const instance = new originalConstructor(...args);
