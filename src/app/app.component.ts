befbc5c5c0943ba37d3d319dfb3e754d81978340
Peter van der Wal <peterzakelijk@live.nl>
2025-01-17 12:18:39 +0100

    Init App root component

---
+++ src/app/app.component.ts
@@ -0,0 +1,7 @@
+import {Component} from "../core/decorators/component.decorator.ts";
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+})
+export class AppComponent {}
